c741b7397231704535360bbf2dab960908ee0375
Sanyamai <129134041+Sanyamai@users.noreply.github.com>
2026-09-14 15:13:06 +0700

    Implement RGB LED control with NeoPixel library
    This code initializes and controls a strip of RGB LEDs using the Adafruit NeoPixel library. It cycles through red, green, and blue colors with a delay of one second between each color.

--- NeoPixel.ino
+++ NeoPixel.ino
@@ -1,1 +1,68 @@
+#include <Adafruit_NeoPixel.h>
 
+// กำหนดขาพอร์ต GPIO27 สำหรับติดต่อกับ LED
+#define PIN BCM27
+
+// กำหนดจำนวนดวง LED RGB ที่ใช้งาน
+#define NUMPIXELS 12
+
+// สร้างออบเจ็กต์ pixels เพื่อควบคุมการแสดงผล
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(
+  NUMPIXELS,
+  PIN,
+  NEO_GRB + NEO_KHZ800
+);
+
+void setup()
+{
+  // เริ่มต้นกระบวนการทำงานของ LED RGB
+  pixels.begin();
+
+  // กำหนดค่าความสว่างระดับ 10 (ช่วง 0–255)
+  pixels.setBrightness(10);
+
+  // ปรับปรุงการแสดงผลล่าสุดของ LED
+  pixels.show();
+}
+
+void loop()
+{
+  // วนแสดงผล LED ให้ครบทุกดวง
+  for (int i = 0; i < NUMPIXELS; i++)
+  {
+    // จับ LED แสดงแสงสีแดง
+    pixels.setPixelColor(i, pixels.Color(255, 0, 0));
+
+    // ปรับปรุงการแสดงผลล่าสุดของ LED
+    pixels.show();
+  }
+
+  // หน่วงเวลา 1 วินาที
+  delay(1000);
+
+  // วนแสดงผล LED ให้ครบทุกดวง
+  for (int i = 0; i < NUMPIXELS; i++)
+  {
+    // จับ LED แสดงแสงสีเขียว
+    pixels.setPixelColor(i, pixels.Color(0, 255, 0));
+
+    // ปรับปรุงการแสดงผลล่าสุดของ LED
+    pixels.show();
+  }
+
+  // หน่วงเวลา 1 วินาที
+  delay(1000);
+
+  // วนแสดงผล LED ให้ครบทุกดวง
+  for (int i = 0; i < NUMPIXELS; i++)
+  {
+    // จับ LED แสดงแสงสีน้ำเงิน
+    pixels.setPixelColor(i, pixels.Color(0, 0, 255));
+
+    // ปรับปรุงการแสดงผลล่าสุดของ LED
+    pixels.show();
+  }
+
+  // หน่วงเวลา 1 วินาที
+  delay(1000);
+}
